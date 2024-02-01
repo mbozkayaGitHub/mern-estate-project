64caa0fdd3d0c081f91fc0ed83e8f5ce7481992b
mbozkayaGitHub <mbozkaya27@gmail.com>
2024-02-01 16:58:29 +0300

    create header compt

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { Link, useNavigate } from 'react-router-dom';
+import { FaSearch } from 'react-icons/fa';
 
 const Header = () => {
   return (
@@ -15,7 +17,7 @@ const Header = () => {
             type="text"
             placeholder="Search..."
             className="bg-transparent focus:outline-none w-24 sm:w-64"
-            value={searchTerm}
+          
           />
           <button>
             <FaSearch className="text-slate-600" />
